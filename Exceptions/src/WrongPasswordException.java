b6745555c72569fabc9031a0d5c94743d70057c7
Sfa11eN <sfallenn@gmail.com>
2024-05-09 22:46:32 +0300

    First commit

---
+++ Exceptions/src/WrongPasswordException.java
@@ -0,0 +1,8 @@
+public class WrongPasswordException extends  Exception{
+    public WrongPasswordException(String message) {
+        super(message);
+    }
+    private static void checkLogin(String login){
+
+    }
+}
