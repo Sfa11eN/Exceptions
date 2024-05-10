711e873bc6cba54e4c42c176c7e026aba2fe081c
Sfa11eN <sfallenn@gmail.com>
2024-05-10 13:17:20 +0300

    Just commit

--- Exceptions/src/WrongLoginException.java
+++
@@ -1,5 +0,0 @@
-public class WrongLoginException extends Exception{
-    public WrongLoginException(String message) {
-        super(message);
-    }
-}
--- Exceptions/src/WrongPasswordException.java
+++
@@ -1,8 +0,0 @@
-public class WrongPasswordException extends  Exception{
-    public WrongPasswordException(String message) {
-        super(message);
-    }
-    private static void checkLogin(String login){
-
-    }
-}
